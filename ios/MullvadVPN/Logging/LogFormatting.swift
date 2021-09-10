8208b48b45b6de45e58eb08137d05f5f4b27b20c
Andrej Mihajlov <and@mullvad.net>
2021-09-10 13:40:06 +0200

    LogFormatting: update

--- ios/MullvadVPN/Logging/LogFormatting.swift
+++ ios/MullvadVPN/Logging/LogFormatting.swift
@@ -8,23 +8,11 @@
 
 import Foundation
 
-extension TimeInterval {
-    func logFormatDuration(allowedUnits: NSCalendar.Unit, unitsStyle: DateComponentsFormatter.UnitsStyle = .full, maximumUnitCount: Int = 1) -> String {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.locale = Locale(identifier: "en_US_POSIX")
-
-        let formatter = DateComponentsFormatter()
-        formatter.calendar = calendar
-        formatter.unitsStyle = unitsStyle
-        formatter.allowedUnits = allowedUnits
-        formatter.maximumUnitCount = maximumUnitCount
-
-        return formatter.string(from: self) ?? "(nil)"
-    }
-}
-
 extension Date {
     func logFormatDate() -> String {
-        return ISO8601DateFormatter().string(from: self)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy @ HH:mm"
+
+        return formatter.string(from: self)
     }
 }
